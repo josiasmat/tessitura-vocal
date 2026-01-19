6f2d9c1e8ae777d0b19f4cb38d15087cd0f6599a
Josias Matschulat <josiasmatschulat@outlook.com>
2026-01-19 15:50:31 -0300

    Fix vite config for GitHub build

--- vite.config.js
+++ vite.config.js
@@ -2,15 +2,15 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 import fs from 'fs';
 
-export default defineConfig({
-	plugins: [sveltekit()],
-	server: {
-		// set https to false to disable ssl locally
-		// https: false,
-		https: {
-			key: fs.readFileSync('./ssl/key.pem'),
-			cert: fs.readFileSync('./ssl/cert.pem')
-		},
-		host: true
-	}	
-});
+export default defineConfig(({ command }) => ({
+    plugins: [sveltekit()],
+    ...(command === 'serve' && {
+        server: {
+            https: {
+                key: fs.readFileSync('./ssl/key.pem'),
+                cert: fs.readFileSync('./ssl/cert.pem')
+            },
+            host: true
+        }
+    })
+}));
