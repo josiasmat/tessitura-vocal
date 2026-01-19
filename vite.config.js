3fb9c49807a0b91921fd0b2b64857e8e2149a6d2
Josias Matschulat <josiasmatschulat@outlook.com>
2026-01-19 15:41:45 -0300

    Use HTTPS for local server

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,16 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import fs from 'fs';
 
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [sveltekit()],
+	server: {
+		// set https to false to disable ssl locally
+		// https: false,
+		https: {
+			key: fs.readFileSync('./ssl/key.pem'),
+			cert: fs.readFileSync('./ssl/cert.pem')
+		},
+		host: true
+	}	
 });
